1792628fe45e54b9806af107ec5b6ce1a3d6808f
taodtu <taodtu@gmail.com>
2021-06-16 12:26:29 +0100

    modify seeding data

--- data/index.js
+++ data/index.js
@@ -1,15 +1,41 @@
-exports.commentData = require("./comments").map(comment => {
-  const { created_at, created_by, belongs_to, ...rest } = comment;
+const format = (arr1, arr2, name, id, keyToChange) => {
+  const lookUp = arr1.reduce((acc, cur) => {
+    acc[cur[name]] = cur[id];
+    return acc;
+  }, {});
+  return arr2.map((e) => {
+    const { [keyToChange]: keyToChanges, ...rest } = e;
+    return { ...rest, [id]: lookUp[e[keyToChange]] };
+  });
+};
+
+const comment = require("./comments");
+const article = require("./articles");
+
+const articleData = article.map((article, index) => {
+  const { created_at, ...rest } = article;
   return {
     ...rest,
-    author: created_by,
-    article: belongs_to,
-    created_at: new Date(comment["created_at"]).toISOString()
+    created_at: new Date(article["created_at"]).toISOString(),
+    article_id: index,
+    votes: Math.floor(Math.random() * Math.floor(50)),
   };
 });
-exports.articleData = require("./articles").map(article => {
-  const { created_at, ...rest } = article;
-  return { ...rest, created_at: new Date(article["created_at"]).toISOString() };
-});
-exports.topicData = require("./topics").map(topic=>({spk:"topic", ...topic}));
+
+exports.commentData = format(
+  articleData,
+  comment.map((comment) => {
+    const { created_at, created_by, ...rest } = comment;
+    return {
+      ...rest,
+      author: created_by,
+      created_at: new Date(comment["created_at"]).toISOString(),
+    };
+  }),
+  "title",
+  "article_id",
+  "belongs_to"
+);
+exports.articleData = articleData;
+exports.topicData = require("./topics");
 exports.userData = require("./users");
--- model/seedTable.js
+++ model/seedTable.js
@@ -1,17 +1,18 @@
-const { topicData } = require("../data/index");
+const { topicData, commentData } = require("../data/index");
 const { DBclient } = require("../connection");
 
 exports.seedTable = async () => {
-  await DBclient.container.delete();
-  await DBclient.database.containers.create(
-    {
-      id: process.env.CONTAINER_PRIMARY,
-      partitionKey: process.env.PARTITION_KEY,
-    },
-    { offerThroughput: 400 }
-  );
-  await Promise.all(
-    topicData.map((topic) => DBclient.container.items.create(topic))
-  );
+  // await DBclient.container.delete();
+  // await DBclient.database.containers.create(
+  //   {
+  //     id: process.env.CONTAINER_PRIMARY,
+  //     partitionKey: process.env.PARTITION_KEY,
+  //   },
+  //   { offerThroughput: 400 }
+  // );
+  // await Promise.all(
+  //   topicData.map((topic) => DBclient.container.items.create(topic))
+  // );
+  console.log(commentData[0]);
   return;
 };
