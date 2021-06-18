597a9cbeaae6b9e366bc2dce0d7d5a4de57d4aba
taodtu <taodtu@gmail.com>
2021-06-18 21:01:55 +0100

    implement update comment

--- model/updateComment.js
+++ model/updateComment.js
@@ -1,39 +1,7 @@
 const { DBclient } = require("../connection");
 
-exports.updateComment = async (spk, { inc_votes }) => {
-  const querySpec = {
-    query: `SELECT * from c 
-            WHERE c.spk = @spk`,
-    parameters: [
-      {
-        name: "@spk",
-        value: spk,
-      },
-    ],
-  };
+exports.updateComment = async (comment) => {
+  await DBclient.container.item(comment.id, comment.spk).replace(comment);
 
-  // read all items in the Items container
-  const { resources } = await DBclient.container.items
-    .query(querySpec)
-    .fetchAll();
-  const article = resources[0];
-  const { id, votes } = article;
-  const articleToUpdate = { ...article, votes: votes + inc_votes };
-
-  const { resource: updatedItem } = await DBclient.container
-    .item(id, spk)
-    .replace(articleToUpdate);
-  const { topic, title, author, created_at, article_id, body, comment_count } =
-    updatedItem;
-
-  return {
-    topic,
-    title,
-    author,
-    created_at,
-    votes: votes + inc_votes,
-    article_id,
-    body,
-    comment_count,
-  };
+  return "update comment successful";
 };
--- patchComment/index.js
+++ patchComment/index.js
@@ -1,13 +1,27 @@
+const { updateComment } = require("../model/updateComment");
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+  if (!req.body) {
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+      status: 400,
+      body: {
+        message: "bad request",
+      },
     };
-}
+  } else {
+    try {
+      const result = await updateComment(req.body);
+      context.res = {
+        status: 200,
+        body: result,
+      };
+    } catch (e) {
+      console.log(e);
+      context.res = {
+        status: 500,
+        body: JSON.stringify({
+          message: "update Comment fails",
+        }),
+      };
+    }
+  }
+};
