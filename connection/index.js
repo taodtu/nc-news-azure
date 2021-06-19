c974d4b11ba37fb69d59e1df16786f5b71c14b4c
taodtu <taodtu@gmail.com>
2021-06-19 12:57:27 +0100

    add container_2 to connection

--- connection/index.js
+++ connection/index.js
@@ -3,9 +3,11 @@ const { CosmosClient } = require("@azure/cosmos");
 const client = new CosmosClient(process.env.DB_CONNECTION);
 const database = client.database(process.env.DB_ID);
 const container = database.container(process.env.CONTAINER_PRIMARY);
+const container_2 = database.container(process.env.CONTAINER_2);
 
 exports.DBclient = {
   client,
   database,
   container,
+  container_2,
 };
